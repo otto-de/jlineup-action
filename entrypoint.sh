28e2f6ec272e6726f76777af7b29677448580c65
Marco Geweke <marco.geweke@gmail.com>
2021-01-20 10:43:40 +0100

    Fix JLineup path in entrypoint!

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "Running JLineup with url $1"
 
-RESULT=$(OPENSSL_CONF=/dev/null java -jar jlineup-cli.jar --url $1)
+RESULT=$(OPENSSL_CONF=/dev/null java -jar /jlineup-cli.jar --url $1)
 
 echo ::set-output name=result::$RESULT
